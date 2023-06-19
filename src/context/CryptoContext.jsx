648ed17c87a9daf664c5ce53e247a896f5a0a2e8
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2023-06-18 18:47:12 -0700

    search finish1

--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -14,7 +14,7 @@ export const CryptoProvider = ({ children }) => {
 
   const getCryptoData = async () => {
     try {
-      const response = await fetch(import.meta.env.VITE_URL);
+      const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinSearch}&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`);
       const data = await response.json();
       setCryptoData(data);    
     } catch (error) {
@@ -24,7 +24,7 @@ export const CryptoProvider = ({ children }) => {
 
   const getSearchResult = async (query) => {
     try {
-      const response = await fetch(`${import.meta.env.VITE_SEARCH}${query}`);
+      const response = await fetch(`https://api.coingecko.com/api/v3/search?query=${query}`);
       const data = await response.json();
       setSearchData(data.coins);    
     } catch (error) {
